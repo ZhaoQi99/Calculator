24ad7361170342e61b4814beebf8f70fc253072b
QiZhao <956361916@qq.com>
2018-09-26 20:34:21 +0800

    Add:show_history_activity

---
+++ app/src/main/java/io/github/zhaoqi99/calculator/ShowHistoryActivity.java
@@ -0,0 +1,13 @@
+package io.github.zhaoqi99.calculator;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+public class ShowHistoryActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_history);
+    }
+}
